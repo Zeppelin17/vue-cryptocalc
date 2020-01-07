539081de35f303084e3e17a8acc4bedfc62ddac2
Zeppelin17 <polmonclus@outlook.com>
2020-01-07 06:59:03 +0100

    Install Bootstrap.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+
 
 Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
