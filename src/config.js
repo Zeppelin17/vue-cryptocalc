4af01a7f0983fcb3600dce227804957dddd94d61
Zeppelin17 <polmonclus@outlook.com>
2020-03-29 07:54:22 +0200

    add comments about config settings

--- src/config.js
+++ src/config.js
@@ -9,14 +9,19 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-03-26 06:35:26 
- * Last modified  : 2020-03-26 06:39:19
+ * Last modified  : 2020-03-29 07:53:30
  */
 
+//timeout for API calls
 export const API_TIMEOUT = 15000
+//check exchanges data every some seconds
 export const EXH_INTERVAL_TIMEOUT = 25000
+//compare exchanges data every some secons
 export const CALC_INTERVAL_CHECK = 5000
 
+//backend API endpoint
 export const API_ENDPOINT = "https://zeppelin17.pythonanywhere.com"
+//exchanges endpoints
 export const BINANCE_TICKER = API_ENDPOINT + "/binance/ticker/"
 export const BITFINEX_TICKER = API_ENDPOINT + "/bitfinex/ticker/"
 export const HITBTC_TICKER = API_ENDPOINT + "/hitbtc/ticker/"
